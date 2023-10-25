267d95ddf2526ddf2317ea1f0e378d0683cdd3f5
julianbopp <julian.bopp@tutanota.com>
2023-10-25 03:38:34 +0200

    Use SIREN to fit lodopab-ct image

--- siren.py
+++ siren.py
@@ -3,6 +3,8 @@ from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 import os
+from dival import get_standard_dataset
+from dival import util
 
 from PIL import Image
 from torchvision.transforms import Resize, Compose, ToTensor, Normalize
@@ -150,10 +152,20 @@ def get_cameraman_tensor(sidelength):
     img = transform(img)
     return img
 
+def get_ct_tensor(sidelength, image):
+    img = Image.fromarray(image)
+    transform = Compose([
+        Resize(sidelength),
+        ToTensor(),
+        Normalize(torch.Tensor([0.5]), torch.Tensor([0.5]))
+    ])
+    img = transform(img)
+    return img
+
 class ImageFitting(Dataset):
-    def __init__(self, sidelength):
+    def __init__(self, sidelength, img):
         super().__init__()
-        img = get_cameraman_tensor(sidelength)
+        self.img = img
         self.pixels = img.permute(1, 2, 0).view(-1, 1)
         self.coords = get_mgrid(sidelength, 2)
 
@@ -165,16 +177,23 @@ class ImageFitting(Dataset):
             
         return self.coords, self.pixels
 
-resolution = 128 
-cameraman = ImageFitting(resolution)
+resolution = 128     
+cameraman = ImageFitting(resolution, get_cameraman_tensor(resolution))
 dataloader = DataLoader(cameraman, batch_size=1, pin_memory=True, num_workers=0)
+
+dataset = get_standard_dataset('lodopab')
+image, ground_truth = dataset.get_sample(0)
+ground_truth = get_ct_tensor(resolution, np.asarray(ground_truth))
+lodopab = ImageFitting(resolution, ground_truth)
+dataloader = DataLoader(lodopab, batch_size=1, pin_memory=True, num_workers=0)
+
 
 img_siren = Siren(in_features=2, out_features=1, hidden_features=resolution, 
                   hidden_layers=3, outermost_linear=True)
 img_siren.cuda()
 
 total_steps = 500 # Since the whole image is our dataset, this just means 500 gradient descent steps.
-steps_til_summary = 10
+steps_til_summary = 50 
 
 optim = torch.optim.Adam(lr=1e-4, params=img_siren.parameters())
 
@@ -185,18 +204,18 @@ for step in range(total_steps):
     model_output, coords = img_siren(model_input)    
     loss = ((model_output - ground_truth)**2).mean()
     
-    if not step % steps_til_summary:
+    if not (step + 1) % steps_til_summary:
         print("Step %d, Total loss %0.6f" % (step, loss))
         img_grad = gradient(model_output, coords)
         img_laplacian = laplace(model_output, coords)
+        fig, axes = plt.subplots(1,3, figsize=(18,6))
+        axes[0].imshow(model_output.cpu().view(resolution,resolution).detach().numpy())
+        axes[1].imshow(img_grad.norm(dim=-1).cpu().view(resolution,resolution).detach().numpy())
+        axes[2].imshow(img_laplacian.cpu().view(resolution,resolution).detach().numpy())
+        plt.show()
 
 
     optim.zero_grad()
     loss.backward()
     optim.step()
-
-fig, axes = plt.subplots(1,3, figsize=(18,6))
-axes[0].imshow(model_output.cpu().view(resolution,resolution).detach().numpy())
-axes[1].imshow(img_grad.norm(dim=-1).cpu().view(resolution,resolution).detach().numpy())
-axes[2].imshow(img_laplacian.cpu().view(resolution,resolution).detach().numpy())
 plt.show()
