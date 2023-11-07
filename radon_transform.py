57d7bcfcf95a03af7a99d329b4576579bd163637
julianbopp <julian.bopp@tutanota.com>
2023-11-07 16:52:02 +0100

    Update radon transform padding

--- radon_transform.py
+++ radon_transform.py
@@ -4,25 +4,32 @@ import typing
 import skimage
 import numpy as np
 import math
+from torch.nn import ConstantPad2d
 
-def radon_transform(image: torch.Tensor, theta: int):
+
+def radon_transform(image: torch.Tensor, theta=None):
+    """Dimension of torch tensor should be 1 x height x width"""
+    if theta is None:
+        theta = 180
+
     # Transform image to Grayscale with single channel
     image = v2.Grayscale(1)(image)
 
-    # Rotate image once by 45 to create padding with 0's
-    image = v2.RandomRotation((45, 45), expand=True)(image)
-    image = v2.RandomRotation((-45,-45), expand=False)(image)
-
     _, height, width = image.shape
-    sinogram = torch.zeros([1, height, theta])
+    diagonal = math.sqrt(2) * max([height, width])
+    pad = [int(math.ceil(diagonal -s)) for s in [height, width]]
+    new_center = [(s + p) // 2 for s, p in zip([height, width], pad)]
+    old_center = [s // 2 for s in [height, width]]
+    pad_before = [nc - oc for oc, nc in zip(old_center, new_center)]
+    pad_width = [(pb, p - pb) for pb, p in zip(pad_before, pad)]
+    pad = ConstantPad2d(pad_width[0]+pad_width[1], 0)
+    image = pad(image)
+    sinogram = torch.zeros([1, image.shape[1], theta])
 
     # Rotate by angle and sum in one dimension
     for i in range(theta):
-        rotated_image = v2.RandomRotation((i,i), expand=False)(image)
+        rotated_image = v2.RandomRotation((-i, -i), expand=False)(image)
         sum = torch.sum(rotated_image, 1)
         sinogram[0][:, i] = sum
 
     return sinogram
-
-
-
