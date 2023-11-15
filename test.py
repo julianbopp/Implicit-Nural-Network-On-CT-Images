f2f3596bfdff8308a84f67c11be33fd1c8510b63
julianbopp <julian.bopp@tutanota.com>
2023-11-15 14:27:04 +0100

    Test batch radon

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import DataLoader
+from skimage.transform import radon
+
 from siren import Siren
 import torch
 from lodopabimage import LodopabImage
+from radon_transform import *
 resolution = 256
 img_siren = Siren(in_features=2, out_features=1, hidden_features=resolution,
                   hidden_layers=3, outermost_linear=True)
@@ -12,5 +18,23 @@ grid = dataset.get_mgrid(2)
 input = torch.zeros(1,2)
 input[0][0] = -2
 input[0][1] = -2
-print(input)
+print(input.shape)
 print(img_siren(input))
+
+radon_batch_output = batch_radon(torch.linspace(-(0.5+1.414)/2,(0.5+1.414)/2,steps=256),img_siren, 40)
+print(radon_batch_output)
+print(radon_batch_output.shape)
+plt.imshow(radon_batch_output.detach().numpy().T)
+plt.show()
+
+dataset = LodopabImage(resolution)
+dataloader = DataLoader(dataset, batch_size=dataset.__len__())
+model_input, ground_truth = next(iter(dataloader))
+
+ground_truth = ground_truth.view(1,resolution,-1)
+ground_truth_image = ground_truth.reshape(resolution,resolution).detach().numpy()
+ground_truth_radon = radon(ground_truth_image, np.arange(180), circle=True)
+ground_truth = torch.from_numpy(ground_truth_radon).unsqueeze(0)
+
+plt.imshow(ground_truth_radon)
+plt.show()
