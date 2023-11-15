b64edb503c82bb66e41e375c2a8b2d2124731550
julianbopp <julian.bopp@tutanota.com>
2023-11-15 10:29:39 +0100

    Start work on batch radon

--- radon_transform.py
+++ radon_transform.py
@@ -17,12 +17,12 @@ def radon_transform(image: torch.Tensor, theta=None):
 
     _, height, width = image.shape
     diagonal = math.sqrt(2) * max([height, width])
-    pad = [int(math.ceil(diagonal -s)) for s in [height, width]]
+    pad = [int(math.ceil(diagonal - s)) for s in [height, width]]
     new_center = [(s + p) // 2 for s, p in zip([height, width], pad)]
     old_center = [s // 2 for s in [height, width]]
     pad_before = [nc - oc for oc, nc in zip(old_center, new_center)]
     pad_width = [(pb, p - pb) for pb, p in zip(pad_before, pad)]
-    pad = ConstantPad2d(pad_width[0]+pad_width[1], 0)
+    pad = ConstantPad2d(pad_width[0] + pad_width[1], 0)
     image = pad(image)
     sinogram = torch.zeros([1, image.shape[1], theta])
 
@@ -33,3 +33,29 @@ def radon_transform(image: torch.Tensor, theta=None):
         sinogram[0][:, i] = sum
 
     return sinogram
+
+
+def batch_radon(t, theta=None, f, L):
+    """
+    t : List of points on radon projection plane
+    theta : List [a, b] a start degree, b end degree
+    f : function to sample from (support on [-1, 1]
+    L : number of sample points on one Line
+    """
+
+    if theta is None:
+        theta = np.arange(180)
+    else:
+        theta = np.arange(theta[0], theta[1])
+
+    # line equation:
+    # (x(z),y(z)) = ( z sin(theta) + s cos(theta), -z cos(theta) + s sin(theta)
+
+    sample_grid =
+
+    for i in theta:
+
+        pass
+
+
+
