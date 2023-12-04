b694cad5abba3d2813ff8e90691a6dd979d7dc5f
julianbopp <julian.bopp@tutanota.com>
2023-12-04 18:45:10 +0100

    Make aware of error in code

--- Tests/train_batch_siren.py
+++ Tests/train_batch_siren.py
@@ -80,7 +80,7 @@ for step in range(training_steps):
             loss.backward()
             optim.step()
 
-    step = step + 1
+    step = step + 1 # WTF?
     print(torch.tensor(loss_total[-180 * coordSet.__len__() :]).mean().item())
 
 torch.save(img_siren.state_dict(), "../img_batch_siren.pt")
