0fabb67cb6121b13547d33e86d011d0b12ca9622
julianbopp <julian.bopp@tutanota.com>
2023-12-19 15:10:51 +0100

    Update train batch spline

--- Training/train_batch_spline.py
+++ Training/train_batch_spline.py
@@ -1,8 +1,8 @@
-import math
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
 import torch
 from matplotlib import pyplot as plt
-from skimage.transform import radon
 from torch.utils.data import DataLoader
+from torchmetrics.audio import SignalNoiseRatio
 
 from DatasetClasses.ParameterSet import AngleSet, CoordSet
 from DatasetClasses.lodopabimage import LodopabImage
@@ -10,57 +10,86 @@ from NeuralNetworks.spline import SplineNetwork
 from RadonTransform.radon_transform import batch_radon_siren
 
 N = 128
-spline_network = SplineNetwork(N)
 
-lodopabSet = LodopabImage(N)
-lodopabLoader = DataLoader(lodopabSet, batch_size=lodopabSet.__len__())
-model_input, lodopabImage = next(iter(lodopabLoader))
-ground_truth_image = lodopabImage.reshape(N, N).detach().numpy()
-ground_truth_radon = radon(ground_truth_image, circle=False)
-ground_truth = torch.from_numpy(ground_truth_radon)
-plt.imshow(ground_truth_radon)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+CIRCLE = True
+
+lodopabSet = LodopabImage(N, pad=CIRCLE)
+padded_N = lodopabSet.padded_resolution
+
+image = lodopabSet.image
+sinogram = lodopabSet.get_radon_transform()
+plt.imshow(image.view(padded_N, padded_N).cpu())
 plt.show()
 
+model_input = lodopabSet.coords
+model_input = model_input.to(device)
+
+spline_network = SplineNetwork(padded_N, circle=CIRCLE)
+spline_network = spline_network.to(device)
 
 angleSet = AngleSet(180, rad=False)
-angleLoader = DataLoader(angleSet, batch_size=10, shuffle=True)
+angleSet.angles = angleSet.angles.to(device)
+angleLoader = DataLoader(angleSet, batch_size=180, shuffle=True)
 
-coordSet = CoordSet(int(N * math.sqrt(2)), circle=False)
-coordLoader = DataLoader(coordSet, batch_size=int((N * math.sqrt(2)) / 4))
+coordSet = CoordSet(padded_N, circle=CIRCLE)
+coordSet.coords = coordSet.coords.to(device)
+coordLoader = DataLoader(coordSet, batch_size=padded_N, shuffle=True)
 
 
 optim = torch.optim.Adam(lr=0.1, params=spline_network.parameters())
 
-sample_points = int(N * math.sqrt(2)) // 2
-training_steps = 100
+sample_points = int(padded_N)
+training_steps = 500
 loss_total = []
 
 for step in range(training_steps):
     print(f"training step = {step}")
+    if step % 10 == 0:
+        model_output, _ = spline_network(model_input)
+        plt.imshow(model_output.reshape(padded_N, padded_N).cpu().detach().numpy())
+        plt.show()
+        plt.imshow(image.reshape(padded_N, padded_N).cpu().detach().numpy())
+        plt.show()
+        snr = SignalNoiseRatio()
+        print(
+            snr(
+                model_output.view(-1, 1).squeeze().cpu(),
+                image.view(-1, 1).squeeze().cpu(),
+            )
+        )
     for angle, angle_idx in angleLoader:
         for coords, coords_idx in coordLoader:
             optim.zero_grad()
 
             radon_output = batch_radon_siren(
-                coords, spline_network, sample_points, theta=angle
+                coords,
+                spline_network,
+                sample_points,
+                theta=-angle,
+                device=device,
+                circle=CIRCLE,
             )
 
             # Create a grid to sample radon ground truth
             # reshape angles to [-1,1]
             reshaped_angle = (angle / 179) * 2 - 1
-            grid = torch.stack(torch.meshgrid(coords, reshaped_angle)).permute(
-                1, 2, 0
-            )  # Shape: (len(angle), len(coords), 2)
-            sampled_radon = lodopabSet.sample_image(
-                grid.unsqueeze(0), torch.from_numpy(ground_truth_radon)
+
+            grid = torch.stack(
+                torch.meshgrid(
+                    reshaped_angle,
+                    coords,
+                    indexing="xy",
+                ),
+                dim=2,
             )
+
+            sampled_radon, _ = lodopabSet.sample_radon(grid.unsqueeze(0))
+            sampled_radon = sampled_radon.squeeze()
+
             loss = ((radon_output - sampled_radon) ** 2).mean()
             loss_total.append(loss.item())
 
             loss.backward()
             optim.step()
-    model_output, _ = spline_network(model_input)
-    if step % 5 == 0:
-        plt.imshow(model_output.reshape(N, N).detach().numpy())
-        plt.show()
     print(torch.tensor(loss_total[-180 * coordSet.__len__() :]).mean().item())
