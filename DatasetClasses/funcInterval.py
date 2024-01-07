f061499ef955d663426a90fcf462de8b2ce6354c
julianbopp <julian.bopp@tutanota.com>
2024-01-07 22:09:35 +0100

    Add helper functions to interval class

--- DatasetClasses/funcInterval.py
+++ DatasetClasses/funcInterval.py
@@ -61,8 +61,51 @@ class FuncInterval:
 
             return interval_a, interval_b
 
+    def join(self, other):
+        if self.__eq__(other) and self.dim != other.dim:
+            if self.dim == "x":
+                joint_interval = FuncInterval(
+                    "xy",
+                    self.start,
+                    self.end,
+                    x_sign=self.x_sign,
+                    y_sign=other.y_sign,
+                    x_dist=self.x_dist,
+                    y_dist=other.y_dist,
+                )
+            elif self.dim == "y":
+                joint_interval = FuncInterval(
+                    "xy",
+                    self.start,
+                    self.end,
+                    x_sign=other.x_sign,
+                    y_sign=self.y_sign,
+                    x_dist=other.x_dist,
+                    y_dist=self.y_dist,
+                )
+            else:
+                raise ValueError("dim not set")
+        else:
+            raise ValueError("Intervals can not be combined")
+
+        return joint_interval
+
+    def modify_length(self, new_start, new_end):
+        return FuncInterval(
+            self.dim,
+            new_start,
+            new_end,
+            x_sign=self.x_sign,
+            y_sign=self.y_sign,
+            x_dist=self.x_dist,
+            y_dist=self.y_dist,
+        )
+
     def __eq__(self, other):
         return self.start == other.start and self.end == other.end
 
     def __lt__(self, other):
         return self.start < other.start
+
+    def __str__(self):
+        return f"({self.start,self.end}), dim: {self.dim}, x_sign: {self.x_sign}, y_sign, {self.y_sign}, x_dist: {self.x_dist}, y_dist: {self.y_dist}"
